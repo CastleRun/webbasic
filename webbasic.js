b6ede215b7f121561ce42b4505835f6c3740be98
CastleRun <786901a@gmail.com>
2015-06-02 19:11:29 -0700

    WebBASIC 1.0
    This is WebBASIC, which is a new scripting language.  However, you have
    to download webbasic.js and put it on your website to use it in your
    website
    To set it up, type this in your page's HTML:
    <script
    src="replace_with_the_folder_that_contains_your_webbasic_script/webbasic.js"></script>
    <script type="webbasic>
    
    </script>
    Note:Your webbasic scripts have to be in the first 10 scripts in your
    page to work.
    Commands:
    ECHO string
    Prints string.  No quotes needed.
    GOTO n
    go to line n
    IF a case b THEN command(not IF)
    If a case b is true, do command.
    Cases:
    *EQUAL: equal to
    *UNEQUAL: unequal to
    *LESS: less than
    *GREATER: greater than
    *GOE: greater than or equal to
    *LOE: less than or equal to
    Prints are in console.

---
+++ webbasic.js
@@ -0,0 +1,64 @@
+//webBASIC
+for(var i=0;i<10;i++){
+  if(document.scripts[i].type=="webbasic"){
+    execute(document.scripts[i]);
+  }
+}
+function execute(script){
+  x=script.text.split("\n");
+  for(var j=0;j<x.length;j++){
+    y=x[j].split(" ");
+    switch(y[0]){
+      case "ECHO":
+        console.log(y[1]);
+        break;
+      case "GOTO":
+        j=parseInt(y[1]);
+        break;
+      case "IF":
+        var c;
+        switch(y[2]){
+          case "EQUAL":
+            k=(parseInt(y[1])==parseInt(y[3]))?true:false;
+            break;
+          case "UNEQUAL":
+            k=(parseInt(y[1])!=parseInt(y[3]))?true:false;
+            break;
+          case "LESS":
+            k=(parseInt(y[1])<parseInt(y[3]))?true:false;
+            break;
+          case "GREATER":
+            k=(parseInt(y[1])>parseInt(y[3]));
+            break;
+          case "GOE":
+            k=(parseInt(y[1])>=parseInt(y[3]))?true:false;
+            break;
+          case "LOE":
+            k=(parseInt(y[1])<=parseInt(y[3]))?true:false;
+            break;
+        }
+        if(k===true){
+              var z=new Array(y.length-4);
+              for(var l=5;l<y.length;l++){
+                z[l-5]=y[l];
+              }
+              switch(z[0]){
+                      case "ECHO":
+                        console.log(z[1]);
+                        break;
+                      case "GOTO":
+                        j=parseInt(z[1]);
+                        break;
+                      default:
+                        console.log("ERROR");
+                        continue;
+              }
+            break;
+            
+            }
+        break;
+      default:
+        console.log("ERROR");
+    }
+  }
+}
